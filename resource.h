48fd647249246be925b165cacf1d5e33c404da90
XQ2022 <114139815+XQ2022@users.noreply.github.com>
2023-05-11 10:12:49 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by VCHelper.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_VCHELPER_DIALOG             102
+#define IDD_PROPPAGE_FILETYPE           106
+#define IDD_PROPPAGE_MEDIUM             106
+#define IDR_MAINFRAME                   128
+#define IDD_PROPPAGE_FINDRESULT         130
+#define IDD_ADDTYPE_DLG                 131
+#define IDR_MENU_FILETYPE               132
+#define IDI_ICON_DESK                   133
+#define IDC_TREE_DIRECTORY              1001
+#define IDC_STATIC_SHEET                1002
+#define IDC_LIST_FILETYPE               1004
+#define IDC_BTN_ADD                     1005
+#define IDC_BTN_DEL                     1006
+#define IDC_EDT_FILETYPE                1007
+#define IDC_EDT_INFO                    1008
+#define IDC_LIST_FINDRESULT             1009
+#define IDC_BTN_DEL_ALL                 1011
+#define IDC_PROGRESS1                   1012
+#define IDC_PROGRESS                    1012
+#define IDC_BTN_STOP                    1013
+#define ID_FILETYPE_CHANGE              32771
+#define ID_FILETYPE_DEL                 32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
